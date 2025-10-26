67966c01fcb084603fea0b6a19fd0037ab81e6b5
Pocho001 <pocholopfez@gmail.com>
2025-10-26 16:56:59 -0600

    Agregada las pantallas (main, datos del usuario) (solo probar)

--- app/src/main/java/com/paquito/fitcam_/MainActivity.kt
+++ app/src/main/java/com/paquito/fitcam_/MainActivity.kt
@@ -1,47 +1,20 @@
 package com.paquito.fitcam_
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.paquito.fitcam_.ui.theme.FitcamTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            FitcamTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Mundo",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.main)
+
+        val boton = findViewById<Button>(R.id.btnIrDatosUsuario)
+        boton.setOnClickListener {
+            val intent = Intent(this, Usuario::class.java)
+            startActivity(intent)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitcamTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/paquito/fitcam_/Usuario.kt
@@ -0,0 +1,11 @@
+package com.paquito.fitcam_
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class Usuario : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.datos_usuario)
+    }
+}
