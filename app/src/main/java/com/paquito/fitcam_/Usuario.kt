614f5f593d7a070a517eba8cc144fd7dfd95f2af
Pocho001 <pocholopfez@gmail.com>
2025-10-27 08:37:10 -0600

    Se agregó los campos para agregar el peso y la altura en la clase DatosUruario con un boton para actrualizar los datos

--- app/src/main/java/com/paquito/fitcam_/Usuario.kt
+++ app/src/main/java/com/paquito/fitcam_/Usuario.kt
@@ -1,11 +1,47 @@
 package com.paquito.fitcam_
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.content.Context
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class Usuario : ComponentActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.datos_usuario)
+
+        val txtPesoActual = findViewById<TextView>(R.id.textPesoActual)
+        val txtAlturaActual = findViewById<TextView>(R.id.textAlturaActual)
+        val editPeso = findViewById<EditText>(R.id.editPeso)
+        val editAltura = findViewById<EditText>(R.id.editAltura)
+        val btnActualizar = findViewById<Button>(R.id.btnActualizar)
+
+        val prefs = getSharedPreferences("DatosUsuario", Context.MODE_PRIVATE)
+        val pesoGuardado = prefs.getFloat("peso", 0f)
+        val alturaGuardada = prefs.getFloat("altura", 0f)
+
+        txtPesoActual.text = "Peso actual: ${if (pesoGuardado != 0f) pesoGuardado else "No registrado"} kg"
+        txtAlturaActual.text = "Altura actual: ${if (alturaGuardada != 0f) alturaGuardada else "No registrada"} m"
+
+        btnActualizar.setOnClickListener {
+            val nuevoPeso = editPeso.text.toString().toFloatOrNull()
+            val nuevaAltura = editAltura.text.toString().toFloatOrNull()
+
+            if(nuevoPeso != null && nuevaAltura != null){
+                val editor = prefs.edit()
+                editor.putFloat("peso", nuevoPeso)
+                editor.putFloat("altura", nuevaAltura)
+                editor.apply()
+
+                txtPesoActual.text = "Peso Actual $nuevoPeso kg"
+                txtAlturaActual.text = "Altura Actual $nuevaAltura m"
+                editPeso.text.clear()
+                editAltura.text.clear()
+            }
+        }
     }
 }
