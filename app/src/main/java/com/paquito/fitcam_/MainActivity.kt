3c36cd30740fa43a2a4de5e2e4a8c09ff5f5f73c
Pocho001 <pocholopfez@gmail.com>
2025-10-30 07:28:48 -0600

    Se empecó el main

--- app/src/main/java/com/paquito/fitcam_/MainActivity.kt
+++ app/src/main/java/com/paquito/fitcam_/MainActivity.kt
@@ -3,6 +3,8 @@ package com.paquito.fitcam_
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
@@ -11,8 +13,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
 
-        val boton = findViewById<Button>(R.id.btnIrDatosUsuario)
-        boton.setOnClickListener {
+        val btnCasa = findViewById<ImageButton>(R.id.btnCasa)
+        val btnFavoritos = findViewById<ImageButton>(R.id.btnFavoritos)
+        val btnPerfil = findViewById<ImageButton>(R.id.btnPerfil)
+
+        btnCasa.setOnClickListener {
+            Toast.makeText(this, "Ya se encuentra en el main", Toast.LENGTH_SHORT).show()
+        }
+
+        btnFavoritos.setOnClickListener {
+            Toast.makeText(this, "Aún en construcción :c", Toast.LENGTH_SHORT).show()
+        }
+
+        btnPerfil.setOnClickListener {
             val intent = Intent(this, Usuario::class.java)
             startActivity(intent)
         }
