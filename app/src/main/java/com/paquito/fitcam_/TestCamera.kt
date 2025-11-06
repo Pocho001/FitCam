967568a849bcc36f5729b613a1ae3a080a46343f
Pocho001 <pocholopfez@gmail.com>
2025-11-06 07:52:13 -0600

    Se movio TestCamera

--- app/src/main/java/com/paquito/fitcam_/TestCamera.kt
+++ app/src/main/java/com/paquito/fitcam_/TestCamera.kt
@@ -235,7 +235,7 @@ class TestCamera : ComponentActivity() {
         }
 
         runOnUiThread {
-            poseOverlay.setExtraText("Abdominales: $abdominalesCompletas")
+            poseOverlay.setExtraText("Sentadillas: $abdominalesCompletas")
         }
     }
 
